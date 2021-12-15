90cbf61e90a2e10cd8d27362b87e96316ef0d518
WijiPe <93677775+WijiPe@users.noreply.github.com>
2021-12-15 00:54:59 -0800

    private wall

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, request, session
 from flask_app.models.user import User
-from flask_app.models.recipe import Recipe
+from flask_app.models.message import Message
 from flask_app import app
 from flask_bcrypt import Bcrypt
 from flask import flash
@@ -11,7 +11,7 @@ bcrypt = Bcrypt(app)
 def index():
     if 'id' not in session:
         return render_template("index.html")
-    return redirect("/result")
+    return redirect("/result/")
 
 @app.post('/register')
 def register():
@@ -26,12 +26,28 @@ def register():
         }
         id = User.register(data)
         session['id'] = id
-        return redirect("/result")
+        return redirect("/result/")
     return redirect('/')
+
+@app.route('/result/')
+def result():
+    if 'id' not in session:
+        return redirect('/')
+    data = {
+        'id': session['id']
+    }
+    results = User.get_by_id(data)
+    messages = Message.get_message(data)
+    users = User.get_all()
+    print(results)
+    return render_template("result.html", results=results, messages=messages, users=users)
 
 @app.post('/login')
 def login():
-    user_in_db = User.get_by_email(request.form)
+    data = { 
+        "email" : request.form["email"] 
+        }
+    user_in_db = User.get_by_email(data)
 
     if not user_in_db:
         flash("Invalid Email/Password",'login')
@@ -40,9 +56,30 @@ def login():
         flash("Invalid Email or Password",'login')
         return redirect('/')
     session['id'] = user_in_db.id
-    return redirect("/result")
+    return redirect("/result/")
 
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect('/')
+
+@app.route('/destroy/message/<int:id>')
+def destroy_message(id):
+    data = {
+        'id': id
+    }
+    check_id = Message.get_message_by_id(data)
+    if check_id.receiver_id == session['id']:
+        Message.destroy_message(data)
+        return redirect('/result/')
+    return render_template("delete_message.html", id=id)
+
+@app.post('/send/message')
+def send_message():
+    data = {
+        'message': request.form["message"],
+        'sender_id': session['id'],
+        'receiver_id': request.form['receiver_id']
+    }
+    Message.save_message(data)
+    return redirect("/result/")
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -2,7 +2,7 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 import re
 
-DB = "recipes_schema"
+DB = "private_wall_schema"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.%]+@[a-zA-Z0-9.!&]+\.[a-zA-Z]+$')
 PASSWORD_REGEX1 = re.compile (r'^.*[A-Z].*')
 PASSWORD_REGEX2 = re.compile (r'^.*[0-9].*')
@@ -22,27 +22,37 @@ class User:
         results = connectToMySQL(DB).query_db(query, data)
         return results
 
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(DB).query_db(query)
+        users1 = []
+        for users in results:
+            users1.append(cls(users))
+        return users1
 
     @classmethod
     def get_by_id(cls,data):
         query = "SELECT * FROM users WHERE id = %(id)s;"
         results = connectToMySQL(DB).query_db(query,data)
-        if results:
-            one_user = cls(results[0])
-            print('this is the dictionary', results[0])
-            print('this is the object', one_user)
-            return one_user
+        if len(results) < 1:
+            return False
+        one_user = cls(results[0])
+        print('this is the dictionary', results[0])
+        print('this is the object', one_user)
+        return one_user
 
     
     @classmethod
     def get_by_email(cls,data):
         query = "SELECT * FROM users WHERE email = %(email)s;"
         results = connectToMySQL(DB).query_db(query,data)
-        if results:
-            one_user = cls(results[0])
-            print('this is the dictionary', results[0])
-            print('this is the object', one_user)
-            return one_user
+        if len(results) < 1:
+            return False
+        one_user = cls(results[0])
+        print('this is the dictionary', results[0])
+        print('this is the object', one_user)
+        return one_user
 
 
 
@@ -60,10 +70,10 @@ class User:
         if len(user['password']) < 8:
             is_valid = False
             flash ('Password must be at least 8 charactors.','register')
-        elif not PASSWORD_REGEX1.match(user['password']):
+        if not PASSWORD_REGEX1.match(user['password']):
             is_valid = False
             flash ('Password must include uppercase letter.','register')
-        elif not PASSWORD_REGEX2.match(user['password']):
+        if not PASSWORD_REGEX2.match(user['password']):
             is_valid = False
             flash ('Password must include number.','register')
         return is_valid    
